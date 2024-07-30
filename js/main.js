e58f2e74851e37118970870f9ba00ba57524c776
puviyan-j <puviyanj49@gmail.com>
2024-07-30 16:08:25 +0530

    remove api key

--- js/main.js
+++ js/main.js
@@ -1,40 +1,41 @@
-const api_key = "api_key=14505f2a2450a1c964c0c838699994ac";
+const api_key = "";
 const api_url = "https://api.themoviedb.org/3/";
 
 const pop = api_url + "discover/movie?sort_by=popularity.desc&" + api_key;
 //const pop = 'https://api.themoviedb.org/3/movie/550?api_key=14505f2a2450a1c964c0c838699994ac';
 
-const imgurl ="https://image.tmdb.org/t/p/w500";
+const imgurl = "https://image.tmdb.org/t/p/w500";
 //  const srurl =api_url + "search/movie?"+api_key;
-const er =api_url+"discover/movie/?certification_country=US&certification=R&sort_by=vote_average.desc&"+api_key;
+const er =
+  api_url +
+  "discover/movie/?certification_country=US&certification=R&sort_by=vote_average.desc&" +
+  api_key;
 
- const srurl ="https://api.themoviedb.org/3/search/movie?api_key=14505f2a2450a1c964c0c838699994ac&query="
- 
-const main =document.getElementById('list');
-const btn =document.getElementById('btn');
-const search = document.getElementById('search');
+const srurl =
+  "https://api.themoviedb.org/3/search/movie?api_key=14505f2a2450a1c964c0c838699994ac&query=";
 
+const main = document.getElementById("list");
+const btn = document.getElementById("btn");
+const search = document.getElementById("search");
 
 getmovies(pop);
 
-
 function getmovies(ur) {
-
-    fetch(ur).then(res => res.json()).then(data => {
-        showmovie(data.results); 
-       
-    })
-    
+  fetch(ur)
+    .then((res) => res.json())
+    .then((data) => {
+      showmovie(data.results);
+    });
 }
-function showmovie(data){
-    main.innerHTML='';
-    data.forEach(mov => {
-        const {title,overview,poster_path,vote_count,vote_average} =mov;
-        const moiveEl=document.createElement("div");
-        moiveEl.classList.add("movie")
-         moiveEl.innerHTML = `
+function showmovie(data) {
+  main.innerHTML = "";
+  data.forEach((mov) => {
+    const { title, overview, poster_path, vote_count, vote_average } = mov;
+    const moiveEl = document.createElement("div");
+    moiveEl.classList.add("movie");
+    moiveEl.innerHTML = `
          <div class="post">
-         <img src="${imgurl+poster_path}" alt="${title}">
+         <img src="${imgurl + poster_path}" alt="${title}">
          
          
 
@@ -51,22 +52,19 @@ function showmovie(data){
             </div>
             </div>
             
-            `
-            main.appendChild(moiveEl);
-        
-    });
+            `;
+    main.appendChild(moiveEl);
+  });
 }
 
-function getcolor(color){
-    if(color >= 8){
-        return "green";
-    }
-    else if(color>=5) {
-        return "orange";
-    }
-    else {
-        return "red";
-    }
+function getcolor(color) {
+  if (color >= 8) {
+    return "green";
+  } else if (color >= 5) {
+    return "orange";
+  } else {
+    return "red";
+  }
 }
 
 // btn.addEventListener('submit',(e)=>{
@@ -80,22 +78,16 @@ function getcolor(color){
 //     else{
 //         getmovies(api_url)
 //     }
-    
-    
 
 // })
-function fun(){
-    const searchterm = search.value ;
-    // console.log(search);
-    if(searchterm!=0){
-        //getmovies(srurl+'&query='+searchterm);
-        getmovies(srurl+searchterm);
-        search.value='';
-    }
-    else{
-        getmovies(er)
-    }
-
+function fun() {
+  const searchterm = search.value;
+  // console.log(search);
+  if (searchterm != 0) {
+    //getmovies(srurl+'&query='+searchterm);
+    getmovies(srurl + searchterm);
+    search.value = "";
+  } else {
+    getmovies(er);
+  }
 }
-    
-
